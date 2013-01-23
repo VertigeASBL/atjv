e1f350fad97c8dd02db549ee37c95878ddee63ae
phenix <trash@henix.be>
2013-01-23 17:19:28 +0100

    Zpip, fabrique et fondation + Début Plugin Twitter

---
+++ plugins/fabrique_auto/twitterSpip/lang/paquet-twitterSpip_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'twitterSpip_description' => 'Permet via un squelette spécial d\'intégrer un flux Twitter publique.',
+	'twitterSpip_nom' => 'Twitter pour Spip',
+	'twitterSpip_slogan' => 'Intégrer un flux Twitter dans vos squelettes',
+);
+
+?>
---
+++ plugins/fabrique_auto/twitterSpip/twitterSpip_fonctions.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Twitter pour Spip
+ * (c) 2013 Didier Debondt
+ * Licence GNU/GPL
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+/*
+ * Un fichier de fonctions permet de definir des elements
+ * systematiquement charges lors du calcul des squelettes.
+ *
+ * Il peut par exemple définir des filtres, critères, balises, …
+ * 
+ */
+
+?>
